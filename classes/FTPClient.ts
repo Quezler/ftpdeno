04b4411577645d74d8b26fedca6de5d7bfab30a1
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-11-30 15:14:03 +0100

    ditto

--- classes/FTPClient.ts
+++ classes/FTPClient.ts
@@ -469,8 +469,9 @@ export class FTPClient implements Deno.Closer {
         }
 
         const listing = await this.commandWithData(Commands.ExList, dirName);
+        process.stdout.write(JSON.stringify(listing) + '\n');
         const entries = listing.trimEnd().split("\r\n");
-        console.log(entries);
+        process.stdout.write(JSON.stringify(entries) + '\n');
         return entries.map(e => this.parseMLST(e));
     }
 
